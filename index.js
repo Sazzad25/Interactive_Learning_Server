68becdeb4be1297043c3ec9b896b32c0c1e25f63
Sazzad Hosen <mdsazzadhosentamim95@gmail.com>
2022-10-26 16:24:03 +0600

    addeed new path

--- index.js
+++ index.js
@@ -1,40 +1,43 @@
 const express = require('express')
-const app = express();
+const app = express()
 const cors = require('cors')
-const port = process.env.PORT || 5000;
+const port = process.env.Port || 5000;
 
 app.use(cors());
 
 const categories = require('./data/categories.json');
-const topics = require('./data/topics.json');
-
+const news = require('./data/topics.json');
 
 app.get('/', (req, res) => {
-    res.send('Course API Running');
-  });
+  res.send('News API Running')
+});
 
-  app.get('/topic-categories', (req, res) => {
-    res.send(categories)
-  });
+app.get('/news-categories', (req, res) => {
+  res.send(categories)
+});
 
-  app.get('/category/:id', (req, res) => {
-    const id = req.params.id;
-    if (id === '08'){
-      res.send(topics);
-    }
-    else{
-      const category_topics = topics.filter(n => n.category_id === id);
-      res.send(category_topics);
-    }
-    
-  })
+app.get('/category/:id', (req, res) => {
+  const id = req.params.id;
+  if (id === '08'){
+    res.send(news);
+  }
+  else{
+    const category_news = news.filter(n => n.category_id === id);
+    res.send(category_news);
+  }
+  
+})
 
-  app.get('/topics/:id', (req, res) => {
-    const id = req.params.id;
-    const selectedTopics = news.find( n => n._id === id);
-    res.send(selectedTopics);
-  })
+app.get('/news', (req, res) =>{
+  res.send(news);
+})
 
-  app.listen(port, () => {
-    console.log(`Topic Details server running ${port}`);
-  })
+app.get('/news/:id', (req, res) => {
+  const id = req.params.id;
+  const selectedNews = news.find( n => n._id === id);
+  res.send(selectedNews);
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+})
